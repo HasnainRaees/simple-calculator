9f4d5e25da044bd702e4fe817e2b6e55e06dc6a0
hasnain raees <hasnainraees07@gmail.com>
2024-03-26 03:18:31 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one operator to perform operatiion",
+        type: "list",
+        name: "operators",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Divided",
+            "Exponentiation",
+            "Modulus",
+            "Percentage",
+        ],
+    },
+]);
+if (answer.operators === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operators === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operators === "Divided") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operators === "Exponentiation") {
+    console.log(answer.firstNumber ** answer.secondNumber);
+}
+else if (answer.operators === "Modulus") {
+    console.log("Reaminder is :", answer.firstNumber % answer.secondNumber);
+}
+else if (answer.operators === "Percentage") {
+    console.log((answer.firstNumber / answer.secondNumber) * 100, "%");
+}
+else {
+    console.log("Please select valid operator");
+}
